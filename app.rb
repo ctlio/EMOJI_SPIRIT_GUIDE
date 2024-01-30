086ef0cb0041a723f52ac35a0f728e62678feb8d
crystal tliourane <129141063+ctlio@users.noreply.github.com>
2024-01-30 21:39:06 +0000

    emoji_app.v1.1

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ get("/prediction") do
 
 emoji = HTTP.get("https://emojihub.yurace.pro/api/all/category/animals-and-nature")
 emoji_string = JSON.parse(emoji.to_s)
-random_emoji = response_string.sample
+random_emoji = emoji_string.sample
 @html = random_emoji.fetch("htmlCode")
 
 advice_url = HTTP.get("https://api.adviceslip.com/advice")
