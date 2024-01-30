c88c6b6c6de8fb3ddf1a65fe65b2a6bba02a3356
crystal tliourane <129141063+ctlio@users.noreply.github.com>
2024-01-30 21:28:51 +0000

    emoji_guide_app.v1

--- README.md
+++ README.md
@@ -1,18 +1,2 @@
-# sinatra-template
-
-Use this repository to create new Sinatra apps. 
-
-Optionally, to use `ActiveRecord` for database operations, add to the `app.rb`:
-
-```ruby
-require "sinatra/activerecord"
-```
-
-And in the `config/environment.rb` file add this code block:
-
-```ruby
-configure do
-  # setup a database connection
-  set(:database, { adapter: "sqlite3", database: "db/development.sqlite3" })
-end
-```
+Get an advice from an emoji spirit guide today~ 
+Just hit the "EMOJI Guide Me!" button to get advice!
--- app.rb
+++ app.rb
@@ -1,9 +1,29 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
+require "date"
+
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  date = Time.now.strftime("%a %e, %R %p")
+  @now = date.to_s
+
+
+ erb(:homepage)
+end
+
+get("/prediction") do
+
+emoji = HTTP.get("https://emojihub.yurace.pro/api/all/category/animals-and-nature")
+emoji_string = JSON.parse(emoji.to_s)
+random_emoji = response_string.sample
+@html = random_emoji.fetch("htmlCode")
+
+advice_url = HTTP.get("https://api.adviceslip.com/advice")
+advice_data = JSON.parse(advice_url).fetch("slip")
+@advice = advice_data.fetch("advice")
+
+
+erb(:prediction)
 end
